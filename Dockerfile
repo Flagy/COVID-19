e49ef589cef17f2586a802a0ac5ab4e0e083e755
Flagy <riccardodidio94@gmail.com>
2020-03-14 01:56:53 +0100

    Revert "text manager, first inline query for text"
    This reverts commit e1bc03cc1069f474372949ad0f67ef2f61f46ae6.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:18.04
 RUN apt-get update
 RUN apt install python3-pip -y
 
+
 RUN pip3 install --upgrade pip
 RUN apt-get install git -y
 RUN git clone https://github.com/Flagy/COVID-19.git
--- TextManagement/TxtManager.py
+++
@@ -1,7 +0,0 @@
-class TxtManager():
-
-    def textualInfoItaly(self, jsonData):
-        msg_str = ""
-        for key, value in jsonData[-1].items():
-            msg_str += str(key) + ': ' + str(value) +'\n'
-        return msg_str
